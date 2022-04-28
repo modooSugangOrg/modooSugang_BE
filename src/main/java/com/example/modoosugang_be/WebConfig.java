8a46c38f94affd87629e12eae48adf4143aaf819
plooox <insukim97@gmail.com>
2022-04-28 12:15:15 +0900

    [🧪 TEST] modoosugang.comd CORS설정

--- src/main/java/com/example/modoosugang_be/WebConfig.java
+++ src/main/java/com/example/modoosugang_be/WebConfig.java
@@ -15,6 +15,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
 //        registry.addMapping("/api/**").allowCredentials(true).allowedOrigins("http://localhost:3000");
-        registry.addMapping("/api/**").allowCredentials(true).allowedOrigins("http://modoosugang.com");
+        registry.addMapping("/api/**").allowCredentials(true).allowedOrigins("http://www.modoosugang.com");
     }
 }
